52f06bee987446a95755916565cd533275232cc9
leonardyeoxl <leonardyeoxl@gmail.com>
2021-09-10 16:05:11 +0800

    add interface

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+type Game interface {
+	generateMineSweeperMap(n int, k int) [][]int
+	generatePlayerMap(n int)
+	displayMap([][]int game_map)
+	checkWon([][]int game_map)
+	checkContinueGame(score int)
+}
+
 func generateMineSweeperMap(n int, k int) [][]int {
 
 }
