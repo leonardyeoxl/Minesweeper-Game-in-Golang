450758628b206db45dfc1ee0ffa9eb168bde02a4
leonardyeoxl <leonardyeoxl@gmail.com>
2021-09-10 16:05:03 +0800

    add skeletion functions and interface

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+func generateMineSweeperMap(n int, k int) [][]int {
+
+}
+
+func generatePlayerMap(n int) {
+
+}
+
+func displayMap([][]int game_map) {
+
+}
+
+func checkWon([][]int game_map) {
+
+}
+
+func checkContinueGame(score int) {
+
+}
+
+
+
+func main() {
+
+}
